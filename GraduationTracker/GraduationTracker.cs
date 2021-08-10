8c69f528e13eb875963934fcdb212dd3aac55965
Satish Chittibomma <sc32762@open.ac.uk>
2021-08-10 13:30:45 +0100

    Refactor changes

---
+++ GraduationTracker/GraduationTracker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker
+{
+    public partial class GraduationTracker
+    {
+        public delegate Tuple<bool, Standing> HasGraduatedDelegate(Diploma d,Student s);
+        
+        public static Tuple<bool, Standing>  HasGraduated(Diploma diploma, Student student)
+        {
+            var credits = 0;
+            var average = 0;
+        //Looping through 
+            foreach (var d in diploma.Requirements)
+            {
+                foreach (var s in student.Courses)
+                {
+                    var requirement = Repository.GetRequirement(d);
+
+                    foreach (var r in requirement.Courses)
+                    {
+                        try
+                        {
+                            if (r == s.Id)
+                            {
+                                average += s.Mark;
+                                if (s.Mark > requirement.MinimumMark)
+                                {
+                                    credits += requirement.Credits;
+                                }
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            throw(e);
+                        }
+                       
+                    }
+                }
+            }
+
+            average = average / student.Courses.Length;
+
+            var standing = average switch
+            {
+                < 50 => Standing.Remedial,
+                < 80 => Standing.Average,
+                < 95 => Standing.MagnaCumLaude,
+                _ => Standing.MagnaCumLaude
+            };
+
+            return standing switch
+            {
+                Standing.Remedial => new Tuple<bool, Standing>(false, standing),
+                Standing.Average => new Tuple<bool, Standing>(true, standing),
+                Standing.SumaCumLaude => new Tuple<bool, Standing>(true, standing),
+                Standing.MagnaCumLaude => new Tuple<bool, Standing>(true, standing),
+                _ => new Tuple<bool, Standing>(false, standing)
+            };
+        }
+    }
+}
---
+++ GraduationTracker/Repository.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker
+{
+    public class Repository
+    {
+        public static Student GetStudent(int id)
+        {
+            var students = GetStudents();
+            Student student = null;
+
+            ////for (int i = 0; i < students.Length; i++)
+            ////{
+            ////    if (id == students[i].Id)
+            ////    {
+            ////        student = students[i];
+            ////    }
+            ////}
+            //return student;
+
+            student = students.FirstOrDefault(s => s.Id == id);
+            return student;
+        }
+
+        public static Diploma GetDiploma(int id)
+        {
+            var diplomas = GetDiplomas();
+            Diploma diploma = null;
+
+            //foreach (var t in diplomas)
+            //{
+            //    if (id == t.Id)
+            //    {
+            //        diploma = t;
+            //    }
+            //}
+            //return diploma;
+            diploma = diplomas.FirstOrDefault(d => d.Id == id); 
+            return diploma;
+
+        }
+
+        public static Requirement GetRequirement(int id)
+        {
+            var requirements = GetRequirements();
+            Requirement requirement = null;
+
+            //for (int i = 0; i < requirements.Length; i++)
+            //{
+            //    if (id == requirements[i].Id)
+            //    {
+            //        requirement = requirements[i];
+            //    }
+            //}
+            //return requirement;
+
+
+            requirement = requirements.FirstOrDefault(r => r.Id == id);
+            return requirement;
+        }
+
+
+        private static Diploma[] GetDiplomas()
+        {
+            return new[]
+            {
+                new Diploma
+                {
+                    Id = 1,
+                    Credits = 4,
+                    Requirements = new int[]{100,102,103,104}
+                }
+            };
+        }
+
+        public static Requirement[] GetRequirements()
+        {   
+                return new[]
+                {
+                    new Requirement{Id = 100, Name = "Math", MinimumMark=50, Courses = new int[]{1}, Credits=1 },
+                    new Requirement{Id = 102, Name = "Science", MinimumMark=50, Courses = new int[]{2}, Credits=1 },
+                    new Requirement{Id = 103, Name = "Literature", MinimumMark=50, Courses = new int[]{3}, Credits=1},
+                    new Requirement{Id = 104, Name = "Physichal Education", MinimumMark=50, Courses = new int[]{4}, Credits=1 }
+                };
+        }
+        private static Student[] GetStudents()
+        {
+            return new[]
+            {
+               new Student
+               {
+                   Id = 1,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=95 },
+                        new Course{Id = 2, Name = "Science", Mark=95 },
+                        new Course{Id = 3, Name = "Literature", Mark=95 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
+                   }
+               },
+               new Student
+               {
+                   Id = 2,
+                   Courses = new Course[]
+                   {
+                        new Course{Id = 1, Name = "Math", Mark=80 },
+                        new Course{Id = 2, Name = "Science", Mark=80 },
+                        new Course{Id = 3, Name = "Literature", Mark=80 },
+                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
+                   }
+               },
+            new Student
+            {
+                Id = 3,
+                Courses = new Course[]
+                {
+                    new Course{Id = 1, Name = "Math", Mark=50 },
+                    new Course{Id = 2, Name = "Science", Mark=50 },
+                    new Course{Id = 3, Name = "Literature", Mark=50 },
+                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
+                }
+            },
+            new Student
+            {
+                Id = 4,
+                Courses = new Course[]
+                {
+                    new Course{Id = 1, Name = "Math", Mark=40 },
+                    new Course{Id = 2, Name = "Science", Mark=40 },
+                    new Course{Id = 3, Name = "Literature", Mark=40 },
+                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
+                }
+            }
+
+            };
+        }
+    }
+
+
+}
